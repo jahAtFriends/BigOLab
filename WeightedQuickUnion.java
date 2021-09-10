a53cbb291df7c4104c4c8ceff1b549a5e1731123
Hammer <jhammer@friendsbalt.org>
2021-09-10 13:13:03 -0400

    swapped QuickUnion for WeightedQuickUnion

---
+++ WeightedQuickUnion.java
@@ -0,0 +1,28 @@
+public class WeightedQuickUnion implements UnionFinder {
+    
+    public UnionFinder(int n) {
+        
+        //TODO: Construct a dynamic connection system of n items.
+        
+    }
+    
+    @Override
+    public void union(int a, int b) {
+        
+        //TODO: Implement this method.
+        
+    }
+    
+    @Override
+    public boolean find(int a, int b) {
+        
+        //TODO: Implement this method.
+        
+    }
+    
+    public static void main(String[] args) {
+        
+        //TODO: Test me!
+        // You can use a scanner to take inputs saved to a file.
+    }
+}
